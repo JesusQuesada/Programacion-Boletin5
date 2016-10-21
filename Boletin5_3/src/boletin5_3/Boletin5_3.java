cf4dd5f36a5a6f8ab891a2c055d3451d1f9b4f75
oracle <oracle@atanasoff32.danielcastelao.org>
2016-10-21 11:52:59 +0200

    Commit

---
+++ Boletin5_3/src/boletin5_3/Boletin5_3.java
@@ -0,0 +1,17 @@
+
+package boletin5_3;
+
+import javax.swing.JOptionPane;
+
+public class Boletin5_3 {
+
+    public static void main(String[] args) {
+        Clase5_3 obj = new Clase5_3();
+        short n1 = Short.parseShort(JOptionPane.showInputDialog("Introducir número 1 "));
+        obj.positivoNegativo(n1);
+                
+    }
+    
+}
+    
+
---
+++ Boletin5_3/src/boletin5_3/Clase5_3.java
@@ -0,0 +1,17 @@
+
+package boletin5_3;
+
+public class Clase5_3 {
+    //atributos
+    short num1;
+    //métodos
+    public void positivoNegativo(short n1){
+        if (n1 > 0)
+            System.out.println("+");
+        else if (n1 < 0)
+            System.out.println("-");
+        else
+            System.out.println("0");
+            
+    }
+}
